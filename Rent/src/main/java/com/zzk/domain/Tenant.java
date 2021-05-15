9025cefc64ae9c6505e8a318da86f058b159e085
zzkya <2933356411@qq.com>
2021-05-15 15:50:48 +0800

    review

---
+++ Rent/src/main/java/com/zzk/domain/Tenant.java
@@ -0,0 +1,12 @@
+package com.zzk.domain;
+
+import java.util.Date;
+
+public class Tenant {
+    int id;
+    String username;
+    String location;
+    int expectedPrice;
+    String expectedInformation;
+    Date time;
+}
