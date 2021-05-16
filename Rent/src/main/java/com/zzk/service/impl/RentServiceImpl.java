20edb860923c7c40c8dadf3d5d1828673e006a7b
zzk <2933356411@qq.com>
2021-05-16 13:37:29 +0800

    service

--- Rent/src/main/java/com/zzk/service/impl/RentServiceImpl.java
+++ Rent/src/main/java/com/zzk/service/impl/RentServiceImpl.java
@@ -1,4 +1,26 @@
 package com.zzk.service.impl;
 
-public class RentServiceImpl {
+import com.zzk.domain.Rent;
+import com.zzk.mapper.RentMapper;
+import com.zzk.service.RentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RentServiceImpl implements RentService {
+
+    @Autowired
+    private RentMapper rentMapper;
+
+    @Override
+    public void save(Rent rent) {
+        rentMapper.save(rent);
+    }
+
+    @Override
+    public List<Rent> findAll() {
+        return rentMapper.findAll();
+    }
 }
